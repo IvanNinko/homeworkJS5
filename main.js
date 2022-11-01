cb419e02bbc921ababff72f85987dfea4c88ebc8
IvanNinko <106483862+Ossievska@users.noreply.github.com>
2022-11-01 19:50:38 +0100

    Initial commit

---
+++ main.js
@@ -0,0 +1,34 @@
+let a = 'hello'
+let b = 'owu'
+let c = 'com'
+let d = 'ua'
+let num1 = '1'
+let num2 = '10'
+let num3 = '-999'
+let num4 = '123'
+let PI = '3.14'
+let num5 = '2.7'
+let num6 = '16'
+let l = 'true'
+let m = 'false'
+console.log(a,b,c,d,num1,num2,num3,num4,PI,num5,num6,l,m)
+
+//let firstName = 'Ivan'
+//let middleName = 'Grigorevich'
+//let lastName = 'Ninko'
+//let pers = firstName + ' ' + middleName + ' ' + lastName
+//console.log(pers)
+
+let z = '100'
+console.log(typeof z)
+let x = 100
+console.log(typeof x)
+let v = true
+console.log(typeof v)
+
+let firstName = prompt('Name')
+let lastName = prompt( 'LastName')
+let age = prompt( 'Age')
+console.log(firstName)
+console.log(lastName)
+console.log(age)
